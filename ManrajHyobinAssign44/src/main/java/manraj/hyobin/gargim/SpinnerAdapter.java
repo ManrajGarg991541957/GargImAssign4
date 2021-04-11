363a1919e66e9d700412371ed775f77f001d7c39
manrajgarg <50338769+manrajgarg@users.noreply.github.com>
2021-04-10 20:24:49 -0400

    Added most of the spinner functionality - running into error where spinner list is not being populated Added images to drawable folder Modified DownloadFrag xml to adhere to assignment instructions

---
+++ ManrajHyobinAssign44/src/main/java/manraj/hyobin/gargim/SpinnerAdapter.java
@@ -0,0 +1,56 @@
+package manraj.hyobin.gargim;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class SpinnerAdapter extends ArrayAdapter<SpinnerItems> {
+
+    public SpinnerAdapter(Context context, ArrayList<SpinnerItems> spinnerList)
+    {
+        super(context, 0, spinnerList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    private View initView(int position, View convertView, ViewGroup parent)
+    {
+        if(convertView == null)
+        {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.team_spinner_row, parent, false
+            );
+        }
+
+        ImageView imageViewTeam = convertView.findViewById(R.id.gargImImage);
+        TextView textViewName = convertView.findViewById(R.id.gargImTvName);
+
+        SpinnerItems currentItem = getItem(position);
+
+        if(currentItem != null)
+        {
+            imageViewTeam.setImageResource(currentItem.getLogoImage());
+            textViewName.setText(currentItem.getImageName());
+        }
+
+        return convertView;
+    }
+}
---
+++ ManrajHyobinAssign44/src/main/java/manraj/hyobin/gargim/SpinnerItems.java
@@ -0,0 +1,23 @@
+package manraj.hyobin.gargim;
+
+public class SpinnerItems {
+
+    private String mImageName;
+    private int mLogoImage;
+
+    public SpinnerItems(String imageName, int logoImage)
+    {
+        mImageName = imageName;
+        mLogoImage = logoImage;
+    }
+
+    public String getImageName()
+    {
+        return mImageName;
+    }
+
+    public int getLogoImage()
+    {
+        return mLogoImage;
+    }
+}
